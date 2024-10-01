51e86628ce80bc229781e03954191515357db5a5
Prathihas <prathihasreddy88@gmail.com>
2024-10-01 13:12:29 -0700

    added About.js component

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section id="about" className="about-section">
+      <h2>About Me</h2>
+      <p>
+        Hi, I'm Prathihas Podduturi, a web developer and software engineer. 
+        I specialize in full-stack development with experience in technologies like Java, Python, React, Spring Boot, and Docker.
+      </p>
+    </section>
+  );
+};
+
+export default About;
